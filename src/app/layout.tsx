a74799de6121fab986c7472b69b4eb1ebd6ce1f0
lsprz2773 <243757@ids.upchiapas.edu.mx>
2026-02-17 23:27:27 -0600

    layout modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,4 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Sidebar from "@/components/organisms/Sidebar";
 
 export default function RootLayout({
   children,
@@ -11,20 +8,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className="antialiased">
-        {/* Contenedor principal flex para dividir la pantalla */}
-        <div className="flex h-screen w-full overflow-hidden">
-          
-          {/*Sidebar persistente */}
-          <aside className="w-64 border-r bg-white dark:bg-slate-900">
-            <Sidebar />
-          </aside>
-
-          {/* El contenido dinámico de rutas (Dashboard, Perfil, etc) */}
-          <main className="flex-1 overflow-y-auto bg-gray-50 dark:bg-slate-950 p-6">
-            {children}
-          </main>
-          
-        </div>
+        {children}
       </body>
     </html>
   );
