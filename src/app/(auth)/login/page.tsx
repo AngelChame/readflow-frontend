f60ca797ffc03ce4174572b8a19b7c2ebf72d80f
lsprz2773 <243757@ids.upchiapas.edu.mx>
2026-02-25 11:15:03 -0600

    replaced some html tags to react components

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
+import {BubbleBackground} from "@/components/animate-ui/components/backgrounds/bubble";
+import {Button} from "@/components/animate-ui/components/buttons/button";
 
 export default function LoginPage() {
     return (
         <main className="py-6 px-10 bg-background min-h-screen w-full h-full flex justify-center">
             <div className="w-full grid grid-cols-[45%_45%] gap-[10%]">
-                <div className="rounded-3xl h-full w-full flex flex-col justify-between px-10 py-16 bg-[url('/gifs/auth.gif')] bg-no-repeat shadow-[0_5px_30px_rgba(91,106,235,0.6)]">
-                    <div className="flex flex-col gap-3.5 text-white">
+                <BubbleBackground interactive={true} colors={{first: "14,23,102", second: "18,29,173", third: "91,138,210", fourth: "101,136,226", fifth: "51,96,187", sixth: "79,187,233"}} className="rounded-3xl h-full w-full flex flex-col justify-between px-10 py-16 shadow-[0_5px_30px_rgba(91,106,235,0.6)]">
+                    <div className="flex flex-col gap-3.5 text-white z-10">
                         <h3 className="text-xl font-light">Con nosotros</h3>
                         <h2 className="text-3xl font-bold">Mejoras cada día</h2>
                     </div>
-                    <div className="flex justify-center text-white">
+                    <div className="flex justify-center text-white z-10">
                         <h2 className="text-3xl font-bold">ReadFlow</h2>
                     </div>
-                </div>
+                </BubbleBackground>
 
                 <div className="grid grid-rows-[auto_1fr] gap-10 pl-20 pr-30 py-24 text-foreground self-center">
                     <div className="flex flex-col gap-3.5">
@@ -28,11 +30,11 @@ export default function LoginPage() {
                             <label htmlFor="password" className="text-lg font-bold">Contraseña <span className="text-red-500">*</span></label>
                             <input id="password" type="password" name="password" className="border border-gray-300 rounded-xl h-10 pl-4" placeholder="Ingresa tu contraseña" required/>
 
-                            <input type="submit" value="Iniciar sesión" className="text-lg py-2 px-10 bg-main-purple text-white font-bold rounded-xl mt-8 w-full self-center cursor-pointer transition-all duration-300 hover:shadow-[0_5px_20px_rgba(91,106,235,0.7)] hover:scale-[1.02]"/>
+                            <Button type="submit" className="text-lg bg-main-purple text-white font-bold rounded-xl mt-8 w-full self-center cursor-pointer transition-all duration-300 hover:shadow-[0_5px_20px_rgba(91,106,235,0.7)] hover:scale-[1.02] hover:bg-main-purple">Iniciar sesión</Button>
                         </form>
 
                         <div className="text-center">
-                            <h4 className="font-light text-foreground">¿Aún no tienes una cuenta? <Link href="/register" className="font-bold text-main-purple transition-all duration-300 hover:text-shadow-[0_5px_20px_rgba(91,106,235,0.7)]">Regístrate</Link></h4>
+                            <h4 className="font-light text-foreground">¿Aún no tienes una cuenta? <Link href="/register"><Button variant={"link"} className="font-bold text-main-purple px-1 hover:text-shadow-[0_5px_20px_rgba(91,106,235,0.7)] hover:cursor-pointer text-base">Regístrate</Button></Link></h4>
                         </div>
                     </div>
                 </div>
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import {BubbleBackground} from "@/components/animate-ui/components/backgrounds/bubble";
+import {Button} from "@/components/animate-ui/components/buttons/button";
 
 export default function RegisterPage() {
     return (
@@ -22,24 +24,24 @@ export default function RegisterPage() {
                             <label htmlFor="password" className="text-lg font-bold">Contraseña <span className="text-red-500">*</span></label>
                             <input id="password" type="password" name="password" className="border border-gray-300 rounded-xl h-10 pl-4" placeholder="Crea una contraseña" required/>
 
-                            <input type="submit" value="Crear cuenta" className="text-lg py-2 px-10 bg-main-purple text-white font-bold rounded-xl mt-8 w-full self-center cursor-pointer transition-all duration-300 hover:shadow-[0_5px_20px_rgba(91,106,235,0.7)] hover:scale-[1.02]"/>
+                            <Button type="submit" className="text-lg py-4 px-10 bg-main-purple text-white font-bold rounded-xl mt-8 w-full self-center cursor-pointer transition-all hover:bg-main-purple duration-300 hover:shadow-[0_5px_20px_rgba(91,106,235,0.7)]">Crear cuenta</Button>
                         </form>
 
                         <div className="text-center">
-                            <h4 className="font-light text-gray-500">¿Ya tienes una cuenta? <Link href="/login" className="font-bold text-main-purple transition-all duration-300 hover:text-shadow-[0_5px_20px_rgba(91,106,235,0.7)]">Inicia sesión</Link></h4>
+                            <h4 className="font-light text-foreground">¿Ya tienes una cuenta? <Link href="/login"><Button variant={"link"} className="hover:text-shadow-[0_5px_20px_rgba(91,106,235,0.7)] px-1 font-bold text-main-purple hover:cursor-pointer text-base">Inicia sesión</Button></Link></h4>
                         </div>
                     </div>
                 </div>
 
-                <div className="rounded-3xl h-full w-full flex flex-col justify-between px-10 py-16 bg-[url('/gifs/auth.gif')] bg-no-repeat shadow-[0_5px_30px_rgba(91,106,235,0.6)]">
-                    <div className="flex flex-col gap-3.5 text-white">
+                <BubbleBackground interactive={true} colors={{first: "14,23,102", second: "18,29,173", third: "91,138,210", fourth: "101,136,226", fifth: "51,96,187", sixth: "79,187,233"}} className="rounded-3xl h-full w-full flex flex-col justify-between px-10 py-16 shadow-[0_5px_30px_rgba(91,106,235,0.6)]">
+                    <div className="flex flex-col gap-3.5 text-white z-10">
                         <h3 className="text-xl font-light">Con nosotros</h3>
                         <h2 className="text-3xl font-bold">Mejoras cada día</h2>
                     </div>
-                    <div className="flex justify-center text-white">
+                    <div className="flex justify-center text-white z-10">
                         <h2 className="text-3xl font-bold">ReadFlow</h2>
                     </div>
-                </div>
+                </BubbleBackground>
             </div>
         </main>
     );
