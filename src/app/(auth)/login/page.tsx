d3c012b8ecdb575ec8c4090ec770384358197532
lsprz2773 <243757@ids.upchiapas.edu.mx>
2026-02-24 23:52:45 -0600

    fixed some styles

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function LoginPage() {
     return (
-        <main className="py-6 px-10 bg-white min-h-screen w-full h-full flex justify-center">
-            <div className="w-full grid grid-cols-[40%_55%] gap-[5%]">
-                <div className="rounded-3xl h-full w-full flex flex-col justify-between px-10 py-16 bg-[url('/gifs/auth.gif')] bg-no-repeat">
+        <main className="py-6 px-10 bg-background min-h-screen w-full h-full flex justify-center">
+            <div className="w-full grid grid-cols-[45%_45%] gap-[10%]">
+                <div className="rounded-3xl h-full w-full flex flex-col justify-between px-10 py-16 bg-[url('/gifs/auth.gif')] bg-no-repeat shadow-[0_5px_30px_rgba(91,106,235,0.6)]">
                     <div className="flex flex-col gap-3.5 text-white">
                         <h3 className="text-xl font-light">Con nosotros</h3>
                         <h2 className="text-3xl font-bold">Mejoras cada día</h2>
@@ -14,7 +14,7 @@ export default function LoginPage() {
                     </div>
                 </div>
 
-                <div className="grid grid-rows-[auto_1fr] gap-10 px-20 py-24 text-black self-center">
+                <div className="grid grid-rows-[auto_1fr] gap-10 pl-20 pr-30 py-24 text-foreground self-center">
                     <div className="flex flex-col gap-3.5">
                         <h2 className="text-5xl font-semibold">Inicia sesión</h2>
                         <p className="text-lg font-light">Bienvenido de nuevo. Mantén tu racha y sigue optimizando tu retención cognitiva.</p>
@@ -28,11 +28,11 @@ export default function LoginPage() {
                             <label htmlFor="password" className="text-lg font-bold">Contraseña <span className="text-red-500">*</span></label>
                             <input id="password" type="password" name="password" className="border border-gray-300 rounded-xl h-10 pl-4" placeholder="Ingresa tu contraseña" required/>
 
-                            <input type="submit" value="Iniciar sesión" className="text-lg py-2 px-10 bg-blue-700 text-white font-bold rounded-xl mt-8 w-fit self-center cursor-pointer transition-all duration-300 hover:shadow-[0_5px_20px_rgba(91,106,235,0.7)] hover:scale-[1.02]"/>
+                            <input type="submit" value="Iniciar sesión" className="text-lg py-2 px-10 bg-main-purple text-white font-bold rounded-xl mt-8 w-full self-center cursor-pointer transition-all duration-300 hover:shadow-[0_5px_20px_rgba(91,106,235,0.7)] hover:scale-[1.02]"/>
                         </form>
 
                         <div className="text-center">
-                            <h4 className="font-light text-gray-500">¿Aún no tienes una cuenta? <Link href="/register" className="font-bold text-blue-700 transition-all duration-300 hover:text-shadow-[0_5px_20px_rgba(91,106,235,0.7)]">Regístrate</Link></h4>
+                            <h4 className="font-light text-foreground">¿Aún no tienes una cuenta? <Link href="/register" className="font-bold text-main-purple transition-all duration-300 hover:text-shadow-[0_5px_20px_rgba(91,106,235,0.7)]">Regístrate</Link></h4>
                         </div>
                     </div>
                 </div>
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function RegisterPage() {
     return (
-        <main className="py-6 px-10 bg-white min-h-screen w-full h-full flex justify-center">
-            <div className="w-full grid grid-cols-[55%_40%] gap-[5%] items-center">
+        <main className="py-6 px-10 bg-background min-h-screen w-full h-full flex justify-center">
+            <div className="w-full grid grid-cols-[45%_45%] gap-[10%] items-center">
 
-                <div className="grid grid-rows-[auto_1fr] gap-10 px-20 py-24 text-black self-center">
+                <div className="grid grid-rows-[auto_1fr] gap-10 pl-30 pr-20 py-24 text-foreground self-center">
                     <div className="flex flex-col gap-3.5">
                         <h2 className="text-5xl font-semibold">Crea tu cuenta</h2>
                         <p className="text-lg font-light">Únete a ReadFlow y transforma cada lectura en un conocimiento permanente.</p>
@@ -22,16 +22,16 @@ export default function RegisterPage() {
                             <label htmlFor="password" className="text-lg font-bold">Contraseña <span className="text-red-500">*</span></label>
                             <input id="password" type="password" name="password" className="border border-gray-300 rounded-xl h-10 pl-4" placeholder="Crea una contraseña" required/>
 
-                            <input type="submit" value="Crear cuenta" className="text-lg py-2 px-10 bg-blue-700 text-white font-bold rounded-xl mt-8 w-fit self-center cursor-pointer transition-all duration-300 hover:shadow-[0_5px_20px_rgba(91,106,235,0.7)] hover:scale-[1.02]"/>
+                            <input type="submit" value="Crear cuenta" className="text-lg py-2 px-10 bg-main-purple text-white font-bold rounded-xl mt-8 w-full self-center cursor-pointer transition-all duration-300 hover:shadow-[0_5px_20px_rgba(91,106,235,0.7)] hover:scale-[1.02]"/>
                         </form>
 
                         <div className="text-center">
-                            <h4 className="font-light text-gray-500">¿Ya tienes una cuenta? <Link href="/login" className="font-bold text-blue-700 transition-all duration-300 hover:text-shadow-[0_5px_20px_rgba(91,106,235,0.7)]">Inicia sesión</Link></h4>
+                            <h4 className="font-light text-gray-500">¿Ya tienes una cuenta? <Link href="/login" className="font-bold text-main-purple transition-all duration-300 hover:text-shadow-[0_5px_20px_rgba(91,106,235,0.7)]">Inicia sesión</Link></h4>
                         </div>
                     </div>
                 </div>
 
-                <div className="rounded-3xl h-full w-full flex flex-col justify-between px-10 py-16 bg-[url('/gifs/auth.gif')] bg-no-repeat">
+                <div className="rounded-3xl h-full w-full flex flex-col justify-between px-10 py-16 bg-[url('/gifs/auth.gif')] bg-no-repeat shadow-[0_5px_30px_rgba(91,106,235,0.6)]">
                     <div className="flex flex-col gap-3.5 text-white">
                         <h3 className="text-xl font-light">Con nosotros</h3>
                         <h2 className="text-3xl font-bold">Mejoras cada día</h2>
